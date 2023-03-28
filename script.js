5eb540d8f70818c8ed3f4f55223a591b89259442
oleg <olegvraby@gmail.com>
2023-03-28 12:03:32 +0200

    change 3 files index.html style.css and script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const backgroundEl = document.querySelector('body');
+const wrapper = document.querySelector('.wrapper');
 const btnEl = document.querySelector('.btn');
 const textEl = document.querySelector('.text');
 const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-changeHex();
+
 btnEl.addEventListener('click', changeHex);
+
 function changeHex() {
   let hex = '#';
   for (let i = 0; i < 6; i++) {
-    const valueHex = Math.floor(Math.random() * hexValues.length);
-    hex += hexValues[valueHex];
-    console.log(hex);
+    hex += hexValues[randomNumber()];
   }
-  backgroundEl.style.backgroundColor = hex;
-  textEl.textContent = `HEX COLOR:${hex}`;
+  wrapper.style.backgroundColor = hex;
+  textEl.textContent = `HEX COLOR: ${hex}`;
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * hexValues.length);
 }
